f676b247542395acde11ac1e024f0906ab9a99d8
Ramsey Chapin <ramsey@Ramseys-MacBook-Pro.local>
2019-09-27 14:34:15 -0700

    first commit

---
+++ lib/CLI_Best_Bars.rb
@@ -0,0 +1,6 @@
+require "CLI_Best_Bars/version"
+
+module CLIBestBars
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/CLI_Best_Bars/version.rb
@@ -0,0 +1,3 @@
+module CLIBestBars
+  VERSION = "0.1.0"
+end
