1733b3fcc43cdc82cada2cb4c2e3d4c249a4c7d7
Ramsey Chapin <ramsey@Ramseys-MacBook-Pro.local>
2019-09-28 11:30:19 -0700

    outlined CLI and started adding functionality

--- lib/CLI_Best_Bars/cli.rb
+++ lib/CLI_Best_Bars/cli.rb
@@ -1,7 +1,28 @@
 class CLIBestBars::CLI
-def call
-  puts "Welcome to your Best Bars concierge. I am here to help you find which of the 50 best bars in the world you should frequent next."
-  binding.pry
-end
+  def call
+    CLIBestBars::Scraper.new.make_bars
+    puts "Welcome to your Best Bars concierge.
+    I am here to help you find which of the 50 best bars in the world you should frequent next."
+    #get_user_bar range or name
+    #get_bar_list
+    #user chooses bar from list
+    start
+  end
+
+  def start
+    puts ""
+    puts "What number bars would you like to see? 1-10, 11-20, 21-30, 31-40, or 41-50?"
+    input = gets.strip.to_i
+
+  end
+
+  def print_bar(bar)
+
+  end
+
+
+  def print_bar(from_number)
+
+  end
 
 end
